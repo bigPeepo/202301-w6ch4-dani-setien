da2ab564496f95c6f0b06d2a1fd35d3be8fbe077
Dani Setien <dani.setien@gmail.com>
2023-02-16 22:05:48 +0100

    Add deleteThing function

--- src/controllers/thingsController.ts
+++ src/controllers/thingsController.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express";
 import { garlicBreadFacts } from "../data/garlicBreadFacts.js";
+import { GarlicBreadFact } from "../data/types.js";
 
 export const getThings = (req: Request, res: Response) => {
   res.status(200).json({ garlicBreadFacts });
@@ -11,4 +12,14 @@ export const getThingDetail = (req: Request, res: Response) => {
   const garlicBreadFact = garlicBreadFacts.find((fact) => fact.id === +id);
 
   res.status(200).json({ garlicBreadFact });
+};
+
+export const deleteThing = (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const targetFact = garlicBreadFacts.find((fact) => fact.id === +id);
+
+  garlicBreadFacts.splice(garlicBreadFacts.indexOf(targetFact!), 1);
+
+  res.status(200).json({ targetFact });
 };
